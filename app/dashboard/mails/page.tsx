75f3a8ca503460ddd930e1d765c0e3bd87721ffa
Lakshay <gupta15.lakshay@gmail.com>
2026-01-07 13:44:18 +0530

    refactor: adjust padding in page layout for improved spacing

--- app/dashboard/mails/page.tsx
+++ app/dashboard/mails/page.tsx
@@ -3,8 +3,8 @@ import TrackedEmail from "../components/TrackedEmail"
 
 const page = () => {
   return (
-    <div className="w-full p-6 space-y-6">
-        <div>
+    <div className="w-full p-4 space-y-6">
+        <div className="px-6">
             <h1 className="text-2xl font-bold tracking-tight">All Tracked Mails</h1>
             <p className="text-muted-foreground">
                 Manage and view all your tracked emails.
