b88eab1086b987f749ec1037dca1f620725df056
Lakshay <gupta15.lakshay@gmail.com>
2026-01-09 11:17:43 +0530

    refactor: simplify subscription data handling in addSubscriptiontoDb function

--- lib/payement.ts
+++ lib/payement.ts
@@ -2,29 +2,31 @@ import { SubscriptionPayload } from "@/types/dodo";
 import { db } from "./prisma";
 
 export async function addSubscriptiontoDb(
-  data:SubscriptionPayload
+  payload: SubscriptionPayload
 ) {
   try {
+
+    const data = payload.data;
     
     await db.subscription.upsert({
-        where:{dodoSubscriptionId: data.data.subscription_id},
+        where:{dodoSubscriptionId: data.subscription_id},
         update:{},
         create:{
-            clerkUserId:data.data.metadata?.clerk_user_id,
-            dodoSubscriptionId:data.data.subscription_id,
-            dodoCustomerId:data.data.customer.customer_id,
-            customerEmail:data.data.customer.email,
-            status:data.data.status,
-            productId:data.data.product_id,
-            currency:data.data.currency,
-            recurringAmount:data.data.recurring_pre_tax_amount,
-            quantity:data.data.quantity,
-            paymentFrequencyInterval:data.data.subscription_period_interval,
-            paymentFrequencyCount:data.data.payment_frequency_count,
-            nextBillingDate: new Date(data.data.next_billing_date),
-            previousBillingDate: new Date(data.data.previous_billing_date),
-            cancelAtNextBillingDate: data.data.cancel_at_next_billing_date,
-            metadata:data.data.metadata || {},
+            clerkUserId:data.metadata?.clerk_user_id,
+            dodoSubscriptionId:data.subscription_id,
+            dodoCustomerId:data.customer.customer_id,
+            customerEmail:data.customer.email,
+            status:data.status,
+            productId:data.product_id,
+            currency:data.currency,
+            recurringAmount:data.recurring_pre_tax_amount,
+            quantity:data.quantity,
+            paymentFrequencyInterval:data.payment_frequency_interval,
+            paymentFrequencyCount:data.payment_frequency_count,
+            nextBillingDate: new Date(data.next_billing_date),
+            previousBillingDate: new Date(data.previous_billing_date),
+            cancelAtNextBillingDate: data.cancel_at_next_billing_date,
+            metadata:data.metadata || {},
         }
 
     })
