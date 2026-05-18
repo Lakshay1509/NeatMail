e77ac628ed86c3a5ab1855f4476cc1d4f5ea29b6
Lakshay Gupta <gupta15.lakshay@gmail.com>
2026-05-18 10:54:47 +0530

    feat: add Cache-Control header to improve caching strategy

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: "/",
+        headers: [
+          { key: "Cache-Control", value: "no-cache, no-store, must-revalidate, private" },
+        ],
+      },
+      {
         source: "/:path*",
         has: [
           {
